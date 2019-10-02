6ce739bda22ebd11035ec43392e3579fd8190908
HB <hbianan@gmail.com>
2019-10-02 10:17:19 -0400

    files updated

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static("."));
 
 app.listen(PORT, function() {
